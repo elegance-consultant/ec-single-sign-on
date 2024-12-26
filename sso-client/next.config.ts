4a2067305a4402e7421d3db89d43a789d050c602
b4nkz-5pch <suppachok.pk007@gmail.com>
2024-12-26 16:01:13 +0700

    fixed-precentProduct

--- sso-client/next.config.ts
+++ sso-client/next.config.ts
@@ -3,10 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "standalone",
-  images: { unoptimized: true },
-  eslint: {
-    ignoreDuringBuilds: true,
-  }
 };
 
 export default nextConfig;
